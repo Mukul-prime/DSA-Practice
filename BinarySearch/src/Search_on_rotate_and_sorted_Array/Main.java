8887e70bab7781c4c77690a1af7cd502f15e98af
CodeByMukul-Tech <mukulvats200@gmail.com>
2026-04-08 18:07:47 +0530

    Backs

--- BinarySearch/src/Search_on_rotate_and_sorted_Array/Main.java
+++ BinarySearch/src/Search_on_rotate_and_sorted_Array/Main.java
@@ -3,20 +3,20 @@ package Search_on_rotate_and_sorted_Array;
 import java.util.*;
 
 class Accio{
-    static int MinElement(int n,int[] arr){
-        int s = 0 ;
-        int e = n-1 ;
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(arr[mid]>arr[mid+1]){
-                return arr[mid];
-            }
-            else if()
-
-
-
-        }
-    }
+//    static int MinElement(int n,int[] arr){
+//        int s = 0 ;
+//        int e = n-1 ;
+//        while(s<=e){
+//            int mid = s+(e-s)/2;
+//            if(arr[mid]>arr[mid+1]){
+//                return arr[mid];
+//            }
+//            else if()
+//
+//
+//
+//        }
+//    }
 }
 
 public class Main {
@@ -28,8 +28,8 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         Accio obj = new Accio();
-        int ans = obj.MinElement(n,arr);
-        System.out.println(ans);
+//        int ans = obj.MinElement(n,arr);
+//        System.out.println(ans);
     }
 }
 
